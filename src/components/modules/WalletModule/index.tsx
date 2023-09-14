c64251f635ffbebff54e811e424b3ed82286f2fd
risalstr <risa.lestari2002@gmail.com>
2023-09-14 13:32:59 +0700

    update wallet

--- src/components/modules/WalletModule/index.tsx
+++ src/components/modules/WalletModule/index.tsx
@@ -1,22 +1,216 @@
-import 'react-toastify/dist/ReactToastify.css'
+// import 'react-toastify/dist/ReactToastify.css'
+// import { useAuthContext } from 'src/components/contexts/AuthContext'
+// import { IAuthContext } from 'src/components/contexts/AuthContext/interface'
+// import Image from 'next/image'
+
+// import { ALink } from '@elements'
+// import { TextInput, Button, Spinner } from 'flowbite-react'
+// import React, { useEffect, useState } from 'react'
+// import { DialogueCard } from '../AuthModule/module-elements/DialogueCard'
+// import { IRegisterData } from '../AuthModule/interface'
+// import { EMPTY_REGISTER_DATA } from '../AuthModule/constant'
+// import axios from 'axios'
+// import { useRouter } from 'next/router'
+// import { ToastContainer, toast } from 'react-toastify'
+// import 'react-toastify/dist/ReactToastify.css'
+
+// import { Dropdown } from 'flowbite'
+// import { Wallet } from './interface'
+// import { WASI } from 'wasi'
+
+// export const WalletModule: React.FC = () => {
+// const [open, setOpen] = useState<Boolean>(false)
+// const [total, setTotal] = useState('')
+// const [jenisTransaksi, setJenisTransaksi] = useState('')
+// const [tanggalTransaksi, setTanggalTransaksi] = useState('')
+// const [deskripsi, setDeskripsi] = useState('')
+// const [chooseTypePengeluaran, setChooseTypePengeluaran] =
+//   useState<Boolean>(false)
+// const [chooseTypePemasukan, setChooseTypePemasukan] = useState<Boolean>(false)
+// const [wallets, setWallets] = useState<Wallet[]>([])
+
+// const handleDeleteWallet = async (id: Number) => {
+//   await axios
+//     .delete(`http://localhost:8000/wallets/wallet/${id}/delete/`)
+//     .then((response) => {
+//       toast.success('Success delete data')
+//     })
+//     .catch((err) => {
+//       console.log('eror')
+//     })
+// }
+
+//   const handleCreateWallet = async () => {
+//     await axios
+//       .post(
+//         'http://localhost:8000/wallets/wallet/',
+//         {
+//           amount_of_money: total,
+//           type: jenisTransaksi,
+//           date: tanggalTransaksi,
+//           description: deskripsi,
+//         }
+//         // config
+//       )
+//       .then((response) => {
+//         // const updatedForums = (vendors || []).map((vendor) => {
+//         //   if (vendor.id === id) {
+//         //     return {
+//         //       ...vendor,
+//         //       // replies: [...vendor.replies, response.data.content],
+//         //     }
+//         //   } else {
+//         //     return vendor
+//         //   }
+//         // })
+//         // setForums(updatedForums)
+//         // setContent('')
+//         // setShowSection(!showSection)
+//         console.log(response.data)
+//         toast.success('Success post data')
+//         // console.log(response.data)
+//       })
+//   }
+
+// useEffect(() => {
+//   axios
+//     .get('http://localhost:8000/wallets/wallet/')
+//     .then((response) => {
+//       console.log('risaaa')
+//       console.log(response.data)
+//       setWallets(response.data)
+//     })
+//     .catch((error) => {
+//       console.error(error)
+//     })
+// }, [wallets])
+
+//   return (
+//     <>
+//       <div className=" bg-blue-light relative w-full py-10 lg:pt-32 md:pt-28 pt-24 lg:px-32 md:px-16 px-3">
+//         <ToastContainer />
+//         <h1 className="text-3xl text-center">Transaksi Baru</h1>
+
+//         <br />
+//         <DialogueCard>
+//           <h2>Total Pemasukan/Pengeluaran</h2>
+// <TextInput
+//   id="phone"
+//   type="text"
+//   placeholder="Contoh: 20000"
+//   required={true}
+//   value={total}
+//   onChange={(e) => setTotal(e.target.value)}
+// />
+// <br />
+// <div className="relative inline-block text-left">
+//   <button
+//     onClick={() => setOpen(!open)}
+//     className="bg-blue-500 text-white px-4 py-2 rounded focus:outline-none focus:shadow-outline hover:bg-blue-600"
+//   >
+//     {chooseTypePengeluaran || chooseTypePemasukan
+//       ? `${chooseTypePemasukan ? 'Pemasukan' : 'Pengeluaran'}`
+//       : 'Jenis Transaksi'}
+//   </button>
+//   {open ? (
+//     <div className="mb-5 origin-top-right right-0 mt-2 w-32 rounded-md shadow-lg">
+//       <div className="rounded-md bg-white shadow-xs">
+//         <a
+//           onClick={() => {
+//             setJenisTransaksi('Pengeluaran')
+//             setChooseTypePengeluaran(!chooseTypePengeluaran)
+//           }}
+//           className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
+//         >
+//           Pengeluaran
+//         </a>
+//         <a
+//           onClick={() => {
+//             setJenisTransaksi('Pemasukan')
+//             setChooseTypePemasukan(!chooseTypePemasukan)
+//           }}
+//           className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
+//         >
+//           Pemasukan
+//         </a>
+//       </div>
+//     </div>
+//   ) : (
+//     ''
+//   )}
+// </div>
+
+// <h2 className="mt-2">Tanggal Transaksi</h2>
+// <TextInput
+//   type="text"
+//   placeholder="Contoh: 2023-03-03"
+//   required={true}
+//   value={tanggalTransaksi}
+//   onChange={(e) => setTanggalTransaksi(e.target.value)}
+// />
+// <br />
+// <h2>Description</h2>
+// <TextInput
+//   type="text"
+//   placeholder="Contoh: Transaksi ini berupa pemasukan"
+//   required={true}
+//   value={deskripsi}
+//   onChange={(e) => setDeskripsi(e.target.value)}
+// />
+// <br />
+
+//           <Button
+//             className="bg-indigo-500"
+//             onClick={() => handleCreateWallet()}
+//             // onClick={(e) => router.push('/auth/login')}
+//           >
+//             Submit
+//           </Button>
+//         </DialogueCard>
+
+//         {wallets.map((wallet) => (
+//           <>
+//             <a
+//               key={wallet.id}
+//               href="#"
+//               className="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700"
+//             >
+//               <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+//                 {wallet.type}
+//               </h5>
+//               <p className="font-normal text-gray-700 dark:text-gray-400">
+//                 {wallet.description}
+//               </p>
+//               <Button
+//                 className="bg-indigo-500"
+//                 onClick={() => handleDeleteWallet(wallet.id)}
+//                 // onClick={(e) => router.push('/auth/login')}
+//               >
+//                 Delete
+//               </Button>
+//             </a>
+//           </>
+//         ))}
+//       </div>
+//     </>
+//   )
+// }
+
+import axios from 'axios'
+import { useEffect, useState } from 'react'
+import Image from 'next/image'
 import { useAuthContext } from 'src/components/contexts/AuthContext'
 import { IAuthContext } from 'src/components/contexts/AuthContext/interface'
-import Image from 'next/image'
-
-import { ALink } from '@elements'
-import { TextInput, Button, Spinner } from 'flowbite-react'
-import React, { useEffect, useState } from 'react'
-import { DialogueCard } from '../AuthModule/module-elements/DialogueCard'
-import { IRegisterData } from '../AuthModule/interface'
-import { EMPTY_REGISTER_DATA } from '../AuthModule/constant'
-import axios from 'axios'
-import { useRouter } from 'next/router'
+import {
+  ChatBubbleBottomCenterTextIcon,
+  HeartIcon,
+} from '@heroicons/react/24/outline'
+import { Wallet } from './interface'
+import { Button, TextInput } from 'flowbite-react'
 import { ToastContainer, toast } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
-
-import { Dropdown } from 'flowbite'
-import { Wallet } from './interface'
-import { WASI } from 'wasi'
+import { DialogueCard } from '../AuthModule/module-elements/DialogueCard'
+import { type } from 'os'
 
 export const WalletModule: React.FC = () => {
   const [open, setOpen] = useState<Boolean>(false)
@@ -41,34 +235,60 @@ export const WalletModule: React.FC = () => {
   }
 
   const handleCreateWallet = async () => {
+    if (
+      total == '' ||
+      jenisTransaksi == '' ||
+      tanggalTransaksi == '' ||
+      deskripsi == ''
+    ) {
+      toast.error('Tidak boleh ada data kosong!')
+    } else {
+      await axios
+        .post(
+          'http://localhost:8000/wallets/wallet/',
+          {
+            amount_of_money: total,
+            type: jenisTransaksi,
+            date: tanggalTransaksi,
+            description: deskripsi,
+          }
+          // config
+        )
+        .then((response) => {
+          // const updatedForums = (vendors || []).map((vendor) => {
+          //   if (vendor.id === id) {
+          //     return {
+          //       ...vendor,
+          //       // replies: [...vendor.replies, response.data.content],
+          //     }
+          //   } else {
+          //     return vendor
+          //   }
+          // })
+          // setForums(updatedForums)
+          // setContent('')
+          // setShowSection(!showSection)
+          console.log(response.data)
+          setTotal('')
+          setJenisTransaksi('')
+          setTanggalTransaksi('')
+          setDeskripsi('')
+          toast.success('Success post data')
+          // console.log(response.data)
+        })
+    }
+  }
+
+  const handleDeleteVendor = async (id: Number) => {
     await axios
-      .post(
-        'http://localhost:8000/wallets/wallet/',
-        {
-          amount_of_money: total,
-          type: jenisTransaksi,
-          date: tanggalTransaksi,
-          description: deskripsi,
-        }
-        // config
-      )
+      .delete(`http://localhost:8000/vendors/vendor/${id}/delete/`)
       .then((response) => {
-        // const updatedForums = (vendors || []).map((vendor) => {
-        //   if (vendor.id === id) {
-        //     return {
-        //       ...vendor,
-        //       // replies: [...vendor.replies, response.data.content],
-        //     }
-        //   } else {
-        //     return vendor
-        //   }
-        // })
-        // setForums(updatedForums)
-        // setContent('')
-        // setShowSection(!showSection)
-        console.log(response.data)
-        toast.success('Success post data')
-        // console.log(response.data)
+        toast.success('Success delete data')
+      })
+      .catch((err) => {
+        toast.error('eror')
+
+        console.log('error')
       })
   }
 
@@ -87,110 +307,142 @@ export const WalletModule: React.FC = () => {
 
   return (
     <>
-      <div className=" bg-blue-light relative w-full py-10 lg:pt-32 md:pt-28 pt-24 lg:px-32 md:px-16 px-3">
-        <ToastContainer />
-        <h1 className="text-3xl text-center">Transaksi Baru</h1>
-
-        <br />
-        <DialogueCard>
-          <h2>Total Pemasukan/Pengeluaran</h2>
-          <TextInput
-            id="phone"
-            type="text"
-            placeholder="Contoh: 20000"
-            required={true}
-            value={total}
-            onChange={(e) => setTotal(e.target.value)}
-          />
-          <br />
-          <div className="relative inline-block text-left">
-            <button
-              onClick={() => setOpen(!open)}
-              className="bg-blue-500 text-white px-4 py-2 rounded focus:outline-none focus:shadow-outline hover:bg-blue-600"
-            >
-              {chooseTypePengeluaran || chooseTypePemasukan
-                ? `${chooseTypePemasukan ? 'Pemasukan' : 'Pengeluaran'}`
-                : 'Jenis Transaksi'}
-            </button>
-            {open ? (
-              <div className="mb-5 origin-top-right right-0 mt-2 w-32 rounded-md shadow-lg">
-                <div className="rounded-md bg-white shadow-xs">
-                  <a
-                    onClick={() => {
-                      setJenisTransaksi('Pengeluaran')
-                      setChooseTypePengeluaran(!chooseTypePengeluaran)
-                    }}
-                    className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
-                  >
-                    Pengeluaran
-                  </a>
-                  <a
-                    onClick={() => {
-                      setJenisTransaksi('Pemasukan')
-                      setChooseTypePemasukan(!chooseTypePemasukan)
-                    }}
-                    className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
-                  >
-                    Pemasukan
-                  </a>
-                </div>
-              </div>
-            ) : (
-              ''
-            )}
-          </div>
-
-          <h2 className="mt-2">Tanggal Transaksi</h2>
-          <TextInput
-            type="text"
-            placeholder="Contoh: 2023-03-03"
-            required={true}
-            value={tanggalTransaksi}
-            onChange={(e) => setTanggalTransaksi(e.target.value)}
-          />
-          <br />
-          <h2>Description</h2>
-          <TextInput
-            type="text"
-            placeholder="Contoh: Transaksi ini berupa pemasukan"
-            required={true}
-            value={deskripsi}
-            onChange={(e) => setDeskripsi(e.target.value)}
-          />
+      <div className="flex flex-col bg-blue-light relative1 justify-center items-center w-full h-full">
+        <div className=" w-10/12 lg:pt-32 md:pt-28 pt-24">
+          <ToastContainer />
           <br />
 
-          <Button
-            className="bg-indigo-500"
-            onClick={() => handleCreateWallet()}
-            // onClick={(e) => router.push('/auth/login')}
-          >
-            Submit
-          </Button>
-        </DialogueCard>
-
-        {wallets.map((wallet) => (
-          <>
-            <a
-              key={wallet.id}
-              href="#"
-              className="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700"
-            >
-              <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-                {wallet.type}
-              </h5>
-              <p className="font-normal text-gray-700 dark:text-gray-400">
-                {wallet.description}
+          <div className="flex lg:flex-row flex-col">
+            <div className="flex flex-col justify-center items-center">
+              <h1 className="py-6 text-3xl  text-display-medium text-purple-light text-center">
+                Wallet Transaksi
+              </h1>
+              <p className="text-center px-6">
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                Tenetur, assumenda?
               </p>
+              <Image
+                width={350}
+                height={350}
+                className="p-8 rounded-t-lg"
+                src="/assets/images/wallet.svg"
+                alt="vendor"
+              />
+            </div>
+            <DialogueCard>
+              <TextInput
+                id="phone"
+                type="text"
+                placeholder="Contoh: 20000"
+                required={true}
+                value={total}
+                onChange={(e) => setTotal(e.target.value)}
+              />
+              <br />
+              <div className="relative inline-block text-left">
+                <button
+                  onClick={() => setOpen(!open)}
+                  className="bg-blue-500 text-white px-4 py-2 rounded focus:outline-none focus:shadow-outline hover:bg-blue-600"
+                >
+                  {chooseTypePengeluaran || chooseTypePemasukan
+                    ? `${chooseTypePemasukan ? 'Pemasukan' : 'Pengeluaran'}`
+                    : 'Jenis Transaksi'}
+                </button>
+                {open ? (
+                  <div className="mb-5 origin-top-right right-0 mt-2 w-32 rounded-md shadow-lg">
+                    <div className="rounded-md bg-white shadow-xs">
+                      <a
+                        onClick={() => {
+                          setJenisTransaksi('Pengeluaran')
+                          setChooseTypePengeluaran(!chooseTypePengeluaran)
+                        }}
+                        className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
+                      >
+                        Pengeluaran
+                      </a>
+                      <a
+                        onClick={() => {
+                          setJenisTransaksi('Pemasukan')
+                          setChooseTypePemasukan(!chooseTypePemasukan)
+                        }}
+                        className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
+                      >
+                        Pemasukan
+                      </a>
+                    </div>
+                  </div>
+                ) : (
+                  ''
+                )}
+              </div>
+
+              <h2 className="mt-2">Tanggal Transaksi</h2>
+              <TextInput
+                type="text"
+                placeholder="Contoh: 2023-03-03"
+                required={true}
+                value={tanggalTransaksi}
+                onChange={(e) => setTanggalTransaksi(e.target.value)}
+              />
+              <br />
+              <h2>Description</h2>
+              <TextInput
+                type="text"
+                placeholder="Contoh: Transaksi ini berupa pemasukan"
+                required={true}
+                value={deskripsi}
+                onChange={(e) => setDeskripsi(e.target.value)}
+              />
+              <br />
+
               <Button
                 className="bg-indigo-500"
-                onClick={() => handleDeleteWallet(wallet.id)}
+                onClick={() => handleCreateWallet()}
                 // onClick={(e) => router.push('/auth/login')}
               >
-                Delete
+                Create New Transaction
               </Button>
-            </a>
-          </>
-        ))}
+            </DialogueCard>
+          </div>
+        </div>
+
+        <div className="w-full">
+          <h1 className="pt-14 text-3xl text-display-medium text-purple-light text-center">
+            Daftar Transaksi
+          </h1>
+          <p className="text-center px-6 mt-5">
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Tenetur,
+            assumenda?
+          </p>
+
+          <div className="lg:flex lg:flex-row md:grid md:grid-cols-3 sm:grid sm:grid-cols-2 lg:p-10 justify-center mx-auto m-10 gap-5 lg:overflow-x-auto lg:w-10/12 md:w-10/12 w-full">
+            {wallets.map((wallet) => (
+              <>
+                <DialogueCard key={wallet.id} className="m-5">
+                  <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                    {wallet.type}
+                  </h5>
+                  <p className="font-normal text-gray-700 dark:text-gray-400">
+                    Jumlah: {wallet.amount_of_money}
+                  </p>
+                  <p className="font-normal text-gray-700 dark:text-gray-400">
+                    Tanggal transaksi: {wallet.date}
+                  </p>
+                  <p className="font-normal text-gray-700 dark:text-gray-400">
+                    Deskripsi: {wallet.description}
+                  </p>
+                  <Button
+                    className="bg-indigo-500"
+                    onClick={() => handleDeleteWallet(wallet.id)}
+                    // onClick={(e) => router.push('/auth/login')}
+                  >
+                    Delete
+                  </Button>
+                </DialogueCard>
+              </>
+            ))}
+          </div>
+        </div>
       </div>
     </>
   )
