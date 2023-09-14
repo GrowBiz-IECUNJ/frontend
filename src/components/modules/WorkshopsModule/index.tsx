a10fcca7da87a35851011a8cde8c403c7b21f485
risalstr <risa.lestari2002@gmail.com>
2023-09-15 00:25:38 +0700

    update workshops

--- src/components/modules/WorkshopsModule/index.tsx
+++ src/components/modules/WorkshopsModule/index.tsx
@@ -3,17 +3,10 @@ import { useEffect, useState } from 'react'
 import Image from 'next/image'
 import { useAuthContext } from 'src/components/contexts/AuthContext'
 import { IAuthContext } from 'src/components/contexts/AuthContext/interface'
-import {
-  ChatBubbleBottomCenterTextIcon,
-  HeartIcon,
-} from '@heroicons/react/24/outline'
 import { Vendor, WorkshopInterface } from './interface'
 import { Button, Spinner, TextInput } from 'flowbite-react'
 import { ToastContainer, toast } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
-import { DialogueCard } from '../AuthModule/module-elements/DialogueCard'
-import { Product } from '../ProductModule/interface'
-import { ProductCard } from '../ProductModule/module-elements/ProductCard'
 import { WorkshopCard } from './module-elements/WorkshopCard'
 
 export const WorkshopsModule: React.FC = () => {
@@ -37,26 +30,14 @@ export const WorkshopsModule: React.FC = () => {
       <div className="flex flex-col bg-blue-light relative1 justify-center items-center w-full h-full">
         <div className=" w-10/12 lg:pt-10 md:pt-28 pt-24">
           <ToastContainer />
-          <br />
 
-          <div className="flex lg:flex-row flex-col justify-center ite">
+          <div className="flex lg:flex-row flex-col justify-center items-center">
             <div className="flex flex-col justify-center items-center">
-              {/* <h1 className="py-6 text-3xl  text-display-medium text-purple-light text-center">
-                Vendor
-              </h1>
-              <p className="text-center px-6">
-                Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-                Tenetur, assumenda?
-              </p> */}
-
-              <h1 className="text-grey-dark text-left lg:text-display-medium text-display-small font-bold mr-3 md:my-0 my-auto">
-                GrowBiz Workshops <p className="inline"></p>
+              <h1 className="text-grey-dark text-center lg:text-left md:mt-20 lg:text-display-medium text-display-small font-bold mr-3 md:my-0 my-auto">
+                GrowBiz Workshops
               </h1>
               <br />
-              <p className=" text-title-medium leading-normal w-10/12">
-                Welcome to Our Forum
-                <br />
-                <br />
+              <p className=" text-title-medium text-center leading-normal w-10/12">
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea
                 atque exercitationem mollitia blanditiis, eveniet harum
                 voluptatibus accusamus fuga fugiat voluptatem adipisci
@@ -69,15 +50,15 @@ export const WorkshopsModule: React.FC = () => {
             <Image
               width={350}
               height={350}
-              className="rounded-t-lg relative drop-shadow-md w-[85%] lg:w-[40%] mx-auto lg:p-12 p-5 rounded-2xl"
+              className="rounded-t-lg relative drop-shadow-md w-[60%] lg:w-[40%] mx-auto lg:p-12 p-5 md:mt-20 rounded-2xl"
               src="/assets/images/workshop.svg"
               alt="workshop"
             />
           </div>
         </div>
 
-        <div className="w-full">
-          <h1 className="pt-14 text-3xl text-display-medium text-purple-light text-center">
+        <div className="w-full mt-14">
+          <h1 className="text-purple-light text-center lg:text-display-medium text-display-small font-bold mr-3 md:my-0 my-auto">
             Daftar Workshops
           </h1>
           <p className="text-center px-6 mt-5">
