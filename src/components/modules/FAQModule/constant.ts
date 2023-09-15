7be8767ceb4099852c80281e5792932515eff748
risalstr <risa.lestari2002@gmail.com>
2023-09-15 20:14:25 +0700

    add faq

---
+++ src/components/modules/FAQModule/constant.ts
@@ -0,0 +1,19 @@
+export const faqs = [
+    {
+      question: 'Apa itu GrowBiz?',
+      answer:
+        'GrowBiz adalah sebuah platform yang bertujuan untuk memberikan solusi dan fitur yang dapat membantu Usaha Mikro, Kecil, dan Menengah (UMKM) mengatasi berbagai kendala yang mereka hadapi dalam mengembangkan bisnis mereka. Platform ini menyediakan berbagai fitur yang dapat membantu UMKM dalam manajemen keuangan, pemasaran, akses modal, literasi keuangan, dan interaksi dengan UMKM lainnya.',
+    },
+    {
+      question: 'Apa saja fitur yang tersedia di GrowBiz?',
+      answer:
+        'GrowBiz menawarkan beberapa fitur utama yang mendukung pertumbuhan Usaha Mikro, Kecil, dan Menengah (UMKM) di Indonesia. Pertama, platform ini memberikan akses modal dengan informasi tentang program inkubasi seperti Antler, membantu UMKM menemukan investor yang sesuai. Kedua, fitur manajemen inventori mempermudah pengelolaan stok produk dan distribusi ke platform e-commerce. Ketiga, fitur manajemen keuangan memungkinkan UMKM mencatat pemasukan dan pengeluaran secara rinci, memberikan wawasan tentang kesehatan finansial. Keempat, terdapat kursus literasi keuangan yang membantu UMKM memahami konsep keuangan dan investasi. Kelima, platform ini memungkinkan interaksi antar-UMKM melalui forum, memungkinkan pertukaran pengalaman dan pembangunan jaringan. Terakhir, profil UMKM dan onboarding membantu dalam membangun kepercayaan dengan pelanggan dan investor. GrowBiz bertujuan untuk memberikan alat dan sumber daya yang dibutuhkan UMKM untuk mengatasi berbagai kendala dalam menjalankan bisnis mereka di Indonesia.',
+    },
+
+    {
+      question: 'Apa saja topik yang dibahas di forum website GrowBiz?',
+      answer:
+        'Forum di website GrowBiz adalah tempat dimana para anggota komunitas UMKM aktif berdiskusi dan berbagi pengetahuan tentang berbagai aspek bisnis. Topik-topik yang dibahas meliputi pengalaman bisnis, strategi pemasaran, manajemen keuangan, akses modal, teknologi, literasi keuangan, serta berita ekonomi dan bisnis. Melalui forum ini, UMKM memiliki kesempatan untuk belajar dari pengalaman orang lain, mendapatkan wawasan tentang praktik bisnis terbaik, dan membangun jaringan dengan sesama pelaku usaha. Ini membantu mereka dalam mengatasi tantangan yang dihadapi dan meningkatkan kualitas bisnis mereka, sehingga mendukung pertumbuhan ekonomi sektor UMKM di Indonesia.',
+    },
+
+  ]
